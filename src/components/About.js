b980056aa44b729edcecb8adc52489c4c139ec7f
ethbru <ethbru@outlook.de>
2022-03-18 15:28:02 +0800

    style

--- src/components/About.js
+++ src/components/About.js
@@ -1,37 +1,40 @@
 import Nav from "./Nav";
 
 function About() {
-    return ( 
+  return (
     <>
-    <Nav />
-            <div className='Page-Title'>
-              <div>
-                <div className="Headline">
-                  
-                  <h1>About etg#1</h1>
-        
-                </div>
-                <p>Hi, I'm Bruno. etg#1 presents my recent artworks and those of 18 friends and artists, most
-                    of which I met along my journey being a digital nomad in the 
-                    Philippines. While the pandemic keeps the world on hold, 
-                    I am having the fortune of feeling greatly inspired by living on the small pacific 
-                    island Siargao for over a year now. Certainly, the inspiration arose from the uncertainty inherent in this
-                    special time the world is experiencing, the beauty, rawness of the island itself,
-                    the simple island life, ultimately amplified by my vivid and intimate social surrounding.
-                    etg#1 was born from this inspiration. Here is to share some of it, and to give 
-                    my very talented friends a humble platform to present their artworks.
-                    Lastly, this exhibition ought to be the first of many, one each year from now!
-                    For each beginning bears a special magic...
-                    </p>
-                    <p>...</p>
-                    <p>If you liked, disliked or don't care about this exhibit, please leave some feedback!</p>
-                    <a className='bigButton startTourButton' href="https://feedbackpostcards.netlify.app/">Feedback</a>
-               
-              </div>
-            </div>
-            </>
-          );
-        };
+      <Nav />
+      <div className='Page-Title'>
+        <div>
+          <div className="Headline">
+
+            <h1>About etg#1</h1>
+
+          </div>
+          <p id="openDate">Open for 4 weeks only <br />
+            February 1, 2021 - February 29, 2021
+          </p>
+          <p>Hello, I am Bruno. etg#1 presents my recent artworks and those of 18 friends and artists, most
+            of which I met along my journey being a digital nomad in the
+            Philippines. While the pandemic keeps the world on hold,
+            I am having the fortune of feeling greatly inspired by living on the small pacific
+            island Siargao for over a year now. Certainly, the inspiration arose from the uncertainty inherent in this
+            special time the world is experiencing, the beauty, rawness of the island,
+            the simple island life, ultimately amplified by my vivid and intimate social surrounding.
+            etg#1 was born from this inspiration. Here is to share some of it, to give
+            my very talented friends a humble platform to present their artworks, and to celebrate the beauty of life.
+            Lastly, this exhibition ought to be the first of many, one each year from now!
+            For each beginning bears a special magic...
+          </p>
+          <p>...</p>
+          <p>If you liked, disliked or don't care about this exhibit, please leave some feedback!</p>
+          <a className='bigButton startTourButton' href="https://feedbackpostcards.netlify.app/">Feedback</a>
+
+        </div>
+      </div>
+    </>
+  );
+};
 
 
 export default About;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useState, useEffect, useContext, useRef } from 'react';
 import { ArtistContext } from '../ArtistContext';
 import { NavLink, Link } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
@@ -8,105 +8,92 @@ import { CgMenu, CgClose } from "react-icons/cg";
 
 
 export default function Nav() {
-  // useEffect(() => {
-  //   console.log('adding event listener');
-  //   document.addEventListener('mousedown', (e) => {
-  //     console.log(e.target.classList)
-  //     if (e.target.classList.contains('dropbtn')) return;
-  //     if (e.target.classList.contains('inactive')) return;
-  //     else {
-  //       document.querySelector('.dropdownContent').classList.toggle('open');
-  //     }
-  //   });
-  // }, []);
+const [artists, getRoomNumber] = useContext(ArtistContext);
+const { room } = useParams();
 
+let index = 0;
+let prev = 0;
+let next = 0;
 
-  const [artists, getRoomNumber] = useContext(ArtistContext);
-  const { room } = useParams();
+if (room) {
+  index = getRoomNumber(room);
+  if (index === 0) { prev = 'all'; next = artists[index + 1].slug; }
+  else if (index === artists.length - 1) { prev = artists[index - 1].slug; next = 'all' }
+  else { prev = artists[index - 1].slug; next = artists[index + 1].slug; }
+}
 
-  let index = 0;
-  let prev = 0;
-  let next = 0;
+const handleToggle = () => {
+  document.querySelector('.navLinks').classList.toggle('show');
+  document.querySelector('.hamburgerToggle').classList.toggle('hide');
+  document.querySelector('.closeToggle').classList.toggle('show');
+}
 
-  if (room) {
-    index = getRoomNumber(room);
-    if (index === 0) { prev = 'all'; next = artists[index + 1].slug; }
-    else if (index === artists.length - 1) { prev = artists[index - 1].slug; next = 'all' }
-    else { prev = artists[index - 1].slug; next = artists[index + 1].slug; }
-  }
+const handleClick = () => {
+  document.querySelector('.dropdownContent').classList.toggle('open');
+}
 
-  const handleToggle = () => {
-    document.querySelector('.navLinks').classList.toggle('show');
-    document.querySelector('.hamburgerToggle').classList.toggle('hide');
-    document.querySelector('.closeToggle').classList.toggle('show');
-  }
+return (
+  <header>
+    {!room && (
+      <nav className='navbar'>
+        <Link className='etg' to='/rooms/all'>etg#1</Link>
+        <div className='navLinks'>
+          <div onClick={handleClick} className='dropdown'>
 
-  const handleClick = () => {
-    document.querySelector('.dropdownContent').classList.toggle('open');
-  }
+            <div className='dropbtn'>Artists ▼</div>
+            <div className='dropdownContent'>
 
-  return (
-    <header>
-      {!room && (
-        <nav className='navbar'>
-          <Link className='etg' to='/rooms/all'>etg#1</Link>
-          <div className='navLinks'>
-            <div onClick={handleClick} className='dropdown'>
-
-              <div className='dropbtn'>Artists ▼</div>
-              <div className='dropdownContent'>
-
-                {artists.map(r => (
-                  <NavLink onClick={handleToggle} key={r.roomN} className={navData => navData.isActive ? 'active' : 'inactive'} to={'/rooms/' + r.slug}>
-                    {r.name} ({r.roomN})
-                  </NavLink>
-                )
-                )}
-
-              </div>
+              {artists.map(r => (
+                <NavLink onClick={handleToggle} key={r.roomN} className={navData => navData.isActive ? 'active' : 'inactive'} to={'/rooms/' + r.slug}>
+                  {r.name} ({r.roomN})
+                </NavLink>
+              )
+              )}
 
             </div>
-            <NavLink onClick={handleToggle} className={navData => navData.isActive ? 'active' : 'inactive'} to='/about'>.!?</NavLink>
+
           </div>
-          <div className="toggleMenu">
-            <CgMenu onClick={handleToggle} className='hamburgerToggle' size={28} />
-            <CgClose onClick={handleToggle} className='closeToggle' size={28} />
-          </div>
-        </nav>
+          <NavLink onClick={handleToggle} className={navData => navData.isActive ? 'active' : 'inactive'} to='/about'>.!?</NavLink>
+        </div>
+        <div className="toggleMenu">
+          <CgMenu onClick={handleToggle} className='hamburgerToggle' size={28} />
+          <CgClose onClick={handleToggle} className='closeToggle' size={28} />
+        </div>
+      </nav>
 
-      )}
-      {room && (
-        <nav className='navbar'>
+    )}
+    {room && (
+      <nav className='navbar'>
 
-          <NavLink className='etg' to='/rooms/all'>etg#1</NavLink>
+        <NavLink className='etg' to='/rooms/all'>etg#1</NavLink>
 
-          <div className='pagination'>
-            <NavLink to={'/rooms/' + prev}>{prev === 'all' ? '' : <TiChevronLeft size={20} />}</NavLink>
-            <p style={{ display: 'block' }}> <span className='paginationNumber' style={index + 1 < 10 ? { padding: '0.2rem .5rem' } : { padding: '0.25rem .3rem' }}>{index + 1}</span><span className='paginationName'>{artists[index].shortName}</span> </p>
-            <NavLink to={'/rooms/' + next}>{next === 'all' ? <AiOutlineRollback size={20} /> : <TiChevronRight size={20} />}</NavLink>
-          </div>
+        <div className='pagination'>
+          <NavLink to={'/rooms/' + prev}>{prev === 'all' ? '' : <TiChevronLeft size={20} />}</NavLink>
+          <p style={{ display: 'block' }}> <span className='paginationNumber' style={index + 1 < 10 ? { padding: '0.2rem .5rem' } : { padding: '0.25rem .3rem' }}>{index + 1}</span><span className='paginationName'>{artists[index].shortName}</span> </p>
+          <NavLink to={'/rooms/' + next}>{next === 'all' ? <AiOutlineRollback size={20} /> : <TiChevronRight size={20} />}</NavLink>
+        </div>
 
-          <div className='navLinks'>
-            <div className='dropdown'>
+        <div className='navLinks'>
+          <div className='dropdown'>
 
-              <div onClick={handleClick} className='dropbtn'>Artists ▼</div>
-              <div className='dropdownContent'>
-                {artists.map(r => (
-                  <NavLink onClick={handleToggle} key={r.roomN} className={navData => navData.isActive ? 'active' : 'inactive'} to={'/rooms/' + r.slug}>
-                    {r.name} ({r.roomN})
-                  </NavLink>
-                )
-                )}
-              </div>
+            <div onClick={handleClick} className='dropbtn'>Artists ▼</div>
+            <div className='dropdownContent'>
+              {artists.map(r => (
+                <NavLink onClick={handleToggle} key={r.roomN} className={navData => navData.isActive ? 'active' : 'inactive'} to={'/rooms/' + r.slug}>
+                  {r.name} ({r.roomN})
+                </NavLink>
+              )
+              )}
             </div>
-            <NavLink onClick={handleToggle} className={navData => navData.isActive ? 'active' : 'inactive'} to='/about'>.!? </NavLink>
           </div>
-          <div className="toggleMenu">
-            <CgMenu onClick={handleToggle} className='hamburgerToggle' size={28} />
-            <CgClose onClick={handleToggle} className='closeToggle' size={28} />
-          </div>
-        </nav>
-      )}
-    </header>
-  )
+          <NavLink onClick={handleToggle} className={navData => navData.isActive ? 'active' : 'inactive'} to='/about'>.!? </NavLink>
+        </div>
+        <div className="toggleMenu">
+          <CgMenu onClick={handleToggle} className='hamburgerToggle' size={28} />
+          <CgClose onClick={handleToggle} className='closeToggle' size={28} />
+        </div>
+      </nav>
+    )}
+  </header>
+)
 };
