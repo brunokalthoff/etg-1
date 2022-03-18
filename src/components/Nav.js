95ba8611e75277d777ea48628b1c4571faf167de
ethbru <ethbru@outlook.de>
2022-03-18 17:53:33 +0800

    ö

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext, useRef } from 'react';
+import React, { useContext } from 'react';
 import { ArtistContext } from '../ArtistContext';
 import { NavLink, Link } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
